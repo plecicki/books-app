0502fcb2f31efc6039e724e79fbe362578dbec0c
Piotr Lecicki <p.lecicki@gmail.com>
2025-02-12 19:12:15 +0100

    Exercise 5

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@
         },
         containerOf: {
             books: '.books-list',
+            filters: '.filters',
         },
         book: {
             image: '.book__image',
@@ -39,40 +40,77 @@
             const generatedHTML = templates.book(thisBook.data);
             thisBook.element = utils.createDOMFromHTML(generatedHTML);
             const booksContainer = document.querySelector(select.containerOf.books);
-            console.log('booksContainer', booksContainer);
             booksContainer.appendChild(thisBook.element);
+        }
+
+        hideIfFiltered(filters) {
+            const thisBook = this;
+            if (filters.includes('adults') && thisBook.data.details.adults) {
+                thisBook.dom.image.classList.add('hidden');
+            } else {
+                thisBook.dom.image.classList.remove('hidden');
+            }
+
+            if (!thisBook.dom.image.classList.contains('hidden')) {
+                if (filters.includes('nonFiction') && thisBook.data.details.nonFiction) {
+                    thisBook.dom.image.classList.add('hidden');
+                } else {
+                    thisBook.dom.image.classList.remove('hidden');
+                }
+            }
         }
     }
 
     const app = {
+        books: [],
         favoriteBooks: [],
+        filters: [],
         initBooks: function () {
             const thisApp = this;
             for (let bookIndex in thisApp.data.books) {
-                new Book(thisApp.data.books[bookIndex]);
+                thisApp.books.push(new Book(thisApp.data.books[bookIndex]));
             }
         },
         initData: function () {
             const thisApp = this;
             thisApp.data = dataSource;
         },
-        initFavoriteAction: function() {
+        initFavoriteAction: function () {
             const thisApp = this;
 
             const booksContainer = document.querySelector(select.containerOf.books);
-            booksContainer.addEventListener('dblclick', function(event) {
+            booksContainer.addEventListener('dblclick', function (event) {
                 event.preventDefault();
                 const clickedGrandpa = event.target.parentElement.parentElement;
-                if(clickedGrandpa.classList.contains('book__image')) {
+                if (clickedGrandpa.classList.contains('book__image')) {
                     const bookId = clickedGrandpa.getAttribute('data-id');
 
-                    if (!app.favoriteBooks.includes(bookId)) {
+                    if (!thisApp.favoriteBooks.includes(bookId)) {
                         clickedGrandpa.classList.add('favorite');
-                        app.favoriteBooks.push(bookId);
+                        thisApp.favoriteBooks.push(bookId);
                     } else {
                         clickedGrandpa.classList.remove('favorite');
-                        app.favoriteBooks.splice(
-                            app.favoriteBooks.indexOf(bookId),1);
+                        thisApp.favoriteBooks.splice(
+                            thisApp.favoriteBooks.indexOf(bookId), 1);
+                    }
+                }
+            });
+        },
+        initFiltersAction: function () {
+            const thisApp = this;
+
+            const filtersContainer = document.querySelector(select.containerOf.filters);
+            filtersContainer.addEventListener('click', function (event) {
+                if (event.target.getAttribute('name') == 'filter' &&
+                    event.target.getAttribute('type') == 'checkbox') {
+                    if (event.target.checked) {
+                        thisApp.filters.push(event.target.value);
+                    } else {
+                        thisApp.filters.splice(
+                            thisApp.filters.indexOf(event.target.value), 1);
+                    }
+                    for (book of thisApp.books) {
+                        book.hideIfFiltered(thisApp.filters);
                     }
                 }
             });
@@ -80,11 +118,10 @@
         init: function () {
             const thisApp = this;
             console.log('*** App starting ***');
-            console.log('thisApp:', thisApp);
-            console.log('templates:', templates);
             thisApp.initData();
             thisApp.initBooks();
             thisApp.initFavoriteAction();
+            thisApp.initFiltersAction();
         },
     }
 
